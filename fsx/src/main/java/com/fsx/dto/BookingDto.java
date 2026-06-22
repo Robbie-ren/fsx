87dcb6a49bf4f7da95173b66d465f9775633b414
Robbie Renita A <renitarobbie@gmail.com>
2026-06-22 21:37:20 +0530

    sonar cube corrections

--- fsx/src/main/java/com/fsx/dto/BookingDto.java
+++ fsx/src/main/java/com/fsx/dto/BookingDto.java
@@ -5,7 +5,7 @@ import com.fsx.enums.BookingStatus;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
+
 
 public record BookingDto(
         int bookingId,
